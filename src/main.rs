e5c70724aeb5f9c54c449e7615962ff91ce070e6
Thej Zain <thejzainkp@gmail.com>
2023-11-03 14:59:47 +0530

    snake is moving hehe

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,20 @@ use piston::event_loop::*;
 use piston::input::*;
 use piston::window::WindowSettings;
 
+enum Direction {
+    Right,
+    Left,
+    Up,
+    Down,
+}
+
 pub struct Game {
     gl: GlGraphics,
-    snake : Snake,
+    snake: Snake,
 }
 
 impl Game {
     fn render(&mut self, arg: &RenderArgs) {
-        use graphics;
-
         let GREEN = [0.0, 1.0, 0.0, 1.0];
 
         self.gl
@@ -25,29 +30,41 @@ impl Game {
 
         self.snake.render(&mut self.gl, arg);
     }
+
+    fn update(&mut self) {
+        self.snake.update()
+    }
 }
 
-struct Snake{
-    pos_x:i32, 
-    pos_y:i32,
+struct Snake {
+    pos_x: i32,
+    pos_y: i32,
+    dir: Direction,
 }
 
 impl Snake {
-    fn render(&self, gl:&mut GlGraphics, args:&RenderArgs){
-        use graphics;
-        let RED = [1.0,0.0,0.0,1.0];
+    fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
+        let RED = [1.0, 0.0, 0.0, 1.0];
 
-        let square = graphics::rectangle::square(self.pos_x as f64, self.pos_y as f64, 20_f64);
+        let square =
+            graphics::rectangle::square((self.pos_x * 20) as f64, (self.pos_y * 20) as f64, 20_f64);
 
-        gl.draw(args.viewport(), |c, gl|{
+        gl.draw(args.viewport(), |c, gl| {
             let transform = c.transform;
 
             graphics::rectangle(RED, square, transform, gl);
         })
     }
+
+    fn update(&mut self) {
+        match self.dir {
+            Direction::Down => self.pos_y += 1,
+            Direction::Left => self.pos_x -= 1,
+            Direction::Right => self.pos_x += 1,
+            Direction::Up => self.pos_y -= 1,
+        }
+    }
 }
-
-
 
 fn main() {
     let opengl = OpenGL::V4_5;
@@ -59,18 +76,22 @@ fn main() {
         .unwrap();
 
     let mut app = Game {
-        gl: GlGraphics::new(opengl), 
-        snake : Snake { pos_x: 50, pos_y: 100 }
+        gl: GlGraphics::new(opengl),
+        snake: Snake {
+            pos_x: 0,
+            pos_y: 0,
+            dir: Direction::Down,
+        },
     };
 
-    let mut events = Events::new(EventSettings::new());
+    let mut events = Events::new(EventSettings::new()).ups(8);
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
             app.render(&args);
         }
 
-        // if let Some(args) = e.update_args() {
-        //     app.update(&args);
-        // }
+        if let Some(args) = e.update_args() {
+            app.update();
+        }
     }
 }
